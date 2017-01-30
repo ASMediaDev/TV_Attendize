8b131f539a01c8a92b07035f7395813ef07a9541
asmediadev <asmediadev@gmail.com>
2017-01-30 09:45:18 +0100

    Initial commit

---
+++ app/Http/Controllers/TicketValAPIController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TicketValAPIController extends Controller
+{
+    public function test()
+    {
+
+        return "Secure via Controller";
+
+    }
+
+    public function getEvents()
+    {
+
+        $events = DB::connection('mysql_attendize')->select('SELECT id, title, start_date, end_date, organiser_id FROM events');
+        $eventarray = array();
+
+        foreach ($events as $event){
+
+            $eventarray[] = $event;
+
+        }
+        echo json_encode($eventarray);
+    }
+
+    public function getAttendees($eventid){
+
+        $attendees = DB::connection('mysql_attendize')->
+        select('SELECT order_id, ticket_id, event_id, first_name, last_name, private_reference_number  FROM attendees WHERE event_id = ?', [$eventid]);
+        $attendeesarray = array();
+
+        foreach ($attendees as $attendee){
+
+            $attendeesarray[] = $attendee;
+
+        }
+        echo json_encode($attendeesarray);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/redirect', function () {
+    $query = http_build_query([
+        'client_id' => 'client-id',
+        'redirect_uri' => 'http://example.com/callback',
+        'response_type' => 'code',
+        'scope' => '',
+    ]);
+
+    return redirect('http://laravel.ticketval.de/oauth/authorize?'.$query);
+});
+
+Route::get('/secured', 'TicketValAPIController@test')->middleware('auth:api');
+
+Route::get('/getEvents', 'TicketValAPIController@getEvents')->middleware('auth:api');
+
+Route::get('/getAttendees/{eventid}', 'TicketValAPIController@getAttendees')->middleware('auth:api');
+
+Route::get('/test', function(){
+
+   return "unsecure";
+});
